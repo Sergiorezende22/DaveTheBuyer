9ebde45fe81b571d411d24918c285b2228c51e07
Sérgio Rezende <sergiorezende2001@gmail.com>
2024-03-08 17:20:19 -0300

    feat: Adicionando configurações do prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+   selector: 'app-root',
+   standalone: true,
+   imports: [RouterOutlet],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'DaveTheBuyer';
-
-  public prepareRoute(outlet: RouterOutlet) {
-    const teste: String = 'Teste';
-
-    teste.length;
-  }
+   title = 'DaveTheBuyer';
 }
