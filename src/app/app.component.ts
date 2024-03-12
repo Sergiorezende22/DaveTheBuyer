74135c49c19be812162e4e6d77d066ce8a4915e6
Sérgio Rezende <sergiorezende2001@gmail.com>
2024-03-11 23:42:22 -0300

    feat: Adding eslint and primeflex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,29 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-   selector: 'app-root',
-   standalone: true,
-   imports: [RouterOutlet, NgxSpinnerModule],
-   templateUrl: './app.component.html',
-   styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgxSpinnerModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-   private primengConfig = inject(PrimeNGConfig);
+    private primengConfig = inject(PrimeNGConfig);
 
-   ngOnInit(): void {
-      this.configurarPrimeNg();
-   }
+    ngOnInit(): void {
+        this.configurarPrimeNg();
+    }
 
-   private configurarPrimeNg(): void {
-      // Habilitar o efeito de ripple
-      this.primengConfig.ripple = true;
+    private configurarPrimeNg(): void {
+        // Habilitar o efeito de ripple
+        this.primengConfig.ripple = true;
 
-      // Configurar zIndex padroes
-      this.primengConfig.zIndex = {
-         modal: 1100, // dialog, sidebar
-         overlay: 1000, // dropdown, overlaypanel
-         menu: 1000, // overlay menus
-         tooltip: 1100, // tooltip
-      };
-   }
+        // Configurar zIndex padroes
+        this.primengConfig.zIndex = {
+            modal: 1100, // dialog, sidebar
+            overlay: 1000, // dropdown, overlaypanel
+            menu: 1000, // overlay menus
+            tooltip: 1100, // tooltip
+        };
+    }
 }
